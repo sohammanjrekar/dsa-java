cd25ae85c73a919161ab657d5e5ab0a4832be290
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-05-27 11:38:28 +0530

    update

---
+++ java/conditional_loop/if_else.java
@@ -0,0 +1,14 @@
+public class if_else{
+    public static void main(String[] args){
+        int age=40;
+        if (age<18) {
+            System.out.println("boy");
+        }
+        else if(age==40){
+            System.out.println("men");
+        }
+        else{
+            System.out.println("adult");
+        }
+    }
+}

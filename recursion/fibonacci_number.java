a7996c0cf6f8c97af63cc16c1fc16697265b0b6a
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-06-04 14:53:52 +0530

    update

--- recursion/fibonacci_number.java
+++ recursion/fibonacci_number.java
@@ -1,16 +1,13 @@
 package recursion;
-
-import java.util.Scanner;
-
 public class fibonacci_number {
     public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-int input=sc.nextInt();
-
+        int ans = fibo(50);
+        System.out.println(ans);
     }
 
     static int fibo(int n) {
-        if (n < 2) {
+        // base condition
+        if (n<2) {
             return n;
         }
         return fibo(n-1) + fibo(n-2);

87dc48c12058d627a6f19b46823e46c25cb27ef4
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-06-07 23:44:22 +0530

    UPDATE

---
+++ string/stream.java
@@ -0,0 +1,32 @@
+package string;
+
+public class stream {
+    public static void main(String[] args) {
+        System.out.println(skipapple("bcappleal"));
+    }
+
+    static String skip(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+        if (up.startsWith("a")) {
+            return skip(up.substring(1));
+        } else {
+            return ch + skip(up.substring(1));
+        }
+    }
+
+    static String skipapple(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        if (up.startsWith("apple")) {
+            return skipapple(up.substring(5));
+        } else {
+            return up.charAt(0)+skipapple(up.substring(1));
+        }
+    }
+}

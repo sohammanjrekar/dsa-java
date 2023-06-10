e503f1b1446d4fe806a9566bb55f46e80776c3ac
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2023-06-10 12:09:47 +0530

    Create nqueen.java

---
+++ backtracking/nqueen.java
@@ -0,0 +1,42 @@
+package backtracking;
+
+public class nqueen {
+    public static void main(String[] args) {
+
+    }
+
+    static int queens(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            return 1;
+        }
+
+        int count = 0;
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+
+        return false;
+    }
+
+    private static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean element : row) {
+                if (element) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.println("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

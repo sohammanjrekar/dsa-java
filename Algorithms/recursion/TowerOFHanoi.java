1e406f87d28f567d7a291bdef1f257f304d37b8f
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2023-06-24 16:32:45 +0530

    update

---
+++ Algorithms/recursion/TowerOFHanoi.java
@@ -0,0 +1,17 @@
+package Algorithms.recursion;
+
+public class TowerOFHanoi {
+    static void Towerofhanoi(int n, String source, String helper, String destination) {
+        if (n == 1) {
+            System.out.println("transfer disk" + n + " from " + source + " to " + destination);
+        }
+        Towerofhanoi(n - 1, source, destination, helper);
+        System.out.println("transfer disk" + n + " from " + source + " to " + destination);
+        Towerofhanoi(n - 1, helper, source, destination);
+    }
+
+    public static void main(String[] args) {
+        int n = 1;
+        Towerofhanoi(n, "S", "H", "D");
+    }
+}

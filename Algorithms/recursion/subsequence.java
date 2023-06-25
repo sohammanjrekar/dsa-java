ce5320508b4a2da17ac4c2fe9b6a030b1709b525
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2023-06-25 19:52:43 +0530

    update

---
+++ Algorithms/recursion/subsequence.java
@@ -0,0 +1,17 @@
+package Algorithms.recursion;
+
+public class subsequence {
+    public static void main(String[] args) {
+        String str = "abc";
+        find_subsequence(str, 0, "");
+    }
+    private static void find_subsequence(String str, int i, String ans) {
+        if (i == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+        char chr = str.charAt(i);
+        find_subsequence(str, i + 1, ans + chr);
+        find_subsequence(str, i + 1, ans);
+    }
+}
